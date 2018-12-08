e02013eb70946882b573cd7daff4770b2d1ca719
Brendon Baughn <45470825+BBaughn100@users.noreply.github.com>
2018-12-08 12:57:31 -0500

    Add files via upload

---
+++ Palidrome.java
@@ -0,0 +1,38 @@
+/*
+ * Check to see if a user input a Palidrome
+ * @author Brendon Baughn
+ * @version 20180920
+ */
+import java.util.Scanner;
+public class Palidrome {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		// make user enter a string
+		System.out.print("Enter a string: ");
+		String pal = input.nextLine();
+		String reverse = "";
+		// loop through until get empty line
+		while (!pal.isEmpty()) {
+			if (pal.length() > 0) {
+				reverse = "";
+				int i;
+				for (i = pal.length() - 1; i >= 0; --i) {
+					reverse = reverse + pal.charAt(i);
+				}
+				if (pal.equals(reverse)) {
+					System.out.println(pal + " is a palidrome");
+					System.out.println();
+				} else {
+					System.out.println(pal + " is not a palidrome");
+					System.out.println();
+				}
+				System.out.print("Enter a string: ");
+				pal = input.nextLine();
+			}
+		}
+		System.out.println("Empty line read - Goodbye!");
+		input.close();
+	}
+
+}
